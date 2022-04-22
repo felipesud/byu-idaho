ae022868bfee593d9386114edf8bb3ef61d14789
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-04-22 11:44:44 -0300

    Add datetime

--- cse-111/week01_Getting_Started/02prove_tire_volume.py
+++ cse-111/week01_Getting_Started/02prove_tire_volume.py
@@ -34,3 +34,8 @@ user_aspect_ratio = float(input('Enter the aspect ratio of the tire (ex 60): '))
 user_diameter = float(input('Enter the diameter of the wheel in inches (ex 15): '))
 
 print(f'\nThe approximate volume is {volume(user_width, user_aspect_ratio, user_diameter):.2f} liters')
+
+from datetime import datetime
+current_date_and_time = datetime.now()
+with open('./assets/volumes.txt', 'at') as volumes_file:
+    print()
