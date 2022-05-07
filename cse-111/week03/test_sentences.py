323bba4ce8e4809528045647dd775fc69286cec4
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-05-07 20:50:08 -0300

    Update Assignment

--- cse-111/week03/test_sentences.py
+++ cse-111/week03/test_sentences.py
@@ -3,10 +3,48 @@ import random
 import pytest
 
 my_range = 5
+single_determiners = ["a", "one", "the"]
+
+plural_determiners = ["two", "some", "many", "the"]
+
+single_noun = ["bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"]
+
+plural_noun = ["birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"]
+
+single_past_verb = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
+
+single_present_verb = ["drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"]
+
+single_future_verb = ["will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
+
+plural_past_verb = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
+
+plural_present_verb = ["drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"]
+
+plural_future_verb = ["will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
+
+prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+
+
 def test_get_determiner():
     # 1. Test the single determiners.
 
-    single_determiners = ["a", "one", "the"]
+    
 
     # This loop will repeat the statements inside it 4 times.
     # If a loop's counting variable is not used inside the
@@ -24,7 +62,7 @@ def test_get_determiner():
 
     # 2. Test the plural determiners.
 
-    plural_determiners = ["two", "some", "many", "the"]
+    
     
 
     # This loop will repeat the statements inside it 4 times.
@@ -42,16 +80,13 @@ def test_get_determiner():
         
 def test_get_noun():
     # Testing SINGLE NOUN
-    single_noun = ["bird", "boy", "car", "cat", "child",
-        "dog", "girl", "man", "rabbit", "woman"]
-
+    
     for _ in range(my_range):
         noun = get_noun(1)
         assert noun in single_noun
     
     # Testing PLURAL NOUN
-    plural_noun = ["birds", "boys", "cars", "cats", "children",
-        "dogs", "girls", "men", "rabbits", "women"]
+   
 
     for _ in range(my_range):
         noun = get_noun(2)
@@ -59,50 +94,42 @@ def test_get_noun():
 
 def test_get_verb():
     # Testing SINGLE PAST verbs
-    single_past_verb = ["drank", "ate", "grew", "laughed", "thought",
-        "ran", "slept", "talked", "walked", "wrote"]
+    
 
     for _ in range(my_range):
         verb = get_verb(1, "past")
         assert verb in single_past_verb
     
     #Testing SINGLE PRESENT verbs
-    single_present_verb = ["drinks", "eats", "grows", "laughs", "thinks",
-        "runs", "sleeps", "talks", "walks", "writes"]
+    
 
     for _ in range(my_range):
         verb = get_verb(1, "present")
         assert verb in single_present_verb 
 
     #Testing SINGLE FUTURE verbs
-    single_future_verb = ["will drink", "will eat", "will grow", "will laugh",
-        "will think", "will run", "will sleep", "will talk",
-        "will walk", "will write"]
+    
 
     for _ in range(my_range):
         verb = get_verb(1, "future")
         assert verb in single_future_verb 
     
     # Testing PLURAL PAST verbs
-    plural_past_verb = ["drank", "ate", "grew", "laughed", "thought",
-        "ran", "slept", "talked", "walked", "wrote"]
+    
 
     for _ in range(my_range):
         verb = get_verb(2, "past")
         assert verb in plural_past_verb
 
     #Testing PLURAL PRESENT verbs
-    plural_present_verb = ["drink", "eat", "grow", "laugh", "think",
-        "run", "sleep", "talk", "walk", "write"]
+    
 
     for _ in range(my_range):
         verb = get_verb(2, "present")
         assert verb in plural_present_verb
     
     #Testing PLURAL FUTURE verbs
-    plural_future_verb = ["will drink", "will eat", "will grow", "will laugh",
-        "will think", "will run", "will sleep", "will talk",
-        "will walk", "will write"]
+   
 
     for _ in range(my_range):
         verb = get_verb(2, "future")
@@ -110,19 +137,42 @@ def test_get_verb():
 
 # Test GETTING PREPOSITIONS
 def test_get_preposition():
-    prepositions = ["about", "above", "across", "after", "along",
-        "around", "at", "before", "behind", "below",
-        "beyond", "by", "despite", "except", "for",
-        "from", "in", "into", "near", "of",
-        "off", "on", "onto", "out", "over",
-        "past", "to", "under", "with", "without"]
+    
     for _ in range(my_range):
         preposition = get_preposition()
         assert preposition in prepositions 
 
 #Test SINGULAR PREPOSITIONAL PHRASES
 def test_get_prepositional_phrase():
-    singular_prepositional = (f'{get_preposition()} {get_determiner(1)} {get_noun(1)}')
+   
+
+   for _ in range(my_range):
+        preposition = get_preposition()
+        determiner =  get_determiner(1)
+        noun = get_noun(1)
+        assert preposition in prepositions
+        assert noun in single_noun
+        assert determiner in single_determiners
+        
+    #Test PLURAL PREPOSITIONAL PHRASES
+    
+        for _ in range(my_range):
+            plural_preposition = get_preposition()
+            plural_determiner =  get_determiner(2)
+            plural_noun = get_noun(2)
+            assert plural_preposition in prepositions
+            assert plural_noun in plural_noun
+            assert plural_determiner in plural_determiners
+
+   
+   
+   
+   
+   
+   
+   
+   
+    
     
 
 
