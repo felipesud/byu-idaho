c122086eae73bb0f01e17faf2ff6da58ca9765e2
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-05-19 20:40:57 -0300

    Add 09 Prove Milestone files

---
+++ cse-111/week05/receipt.py
@@ -0,0 +1,19 @@
+# 09 Prove Milestone: Text Files
+# From: https://byui-cse.github.io/cse111-course/lesson09/prove.html
+# By: Felipe dos Santos Belisário  - https://fsbelisario.com.br/
+
+"""
+Assignment
+During this milestone, you will write half of a Python program named receipt.py that prints to the terminal window a receipt for a customer's grocery order. Specifically, by the end of this milestone, your program must contain at least these two functions:
+
+1. main
+2. read_dict
+and must read and process these two CSV files:
+
+-> The products.csv file is a catalog of all the products that the grocery store sells.
+-> The request.csv file contains the items ordered by a customer.
+
+
+
+
+"""
