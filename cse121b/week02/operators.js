001c11c424e927d8a2d11015b03651c7350515f1
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-09-20 20:33:27 -0300

    1. Add two numbers

---
+++ cse121b/week02/operators.js
@@ -0,0 +1,21 @@
+/* Write a Javascript snippet to do each of the following. Make your best guess if you don't know. Make sure to put some sort of response on each:
+
+
+2. Multiply two numbers
+3. Subtract one from a number
+4. concatinate two strings together.
+5. assign a value to a variable
+6. increment the value in a variable by 3
+7. compare two values to see if they are the same
+8. check to see if one number is less than another number
+9. Check to see if two values are NOT equal
+10. check to see if a value is less than 10 and greater than 0
+*/
+
+// 1. Add two numbers
+
+const number1 = 5
+const number2 = 10
+const totalSum = number1 + number2
+console.log(`1. Add two numbers = ${totalSum}`)
+
