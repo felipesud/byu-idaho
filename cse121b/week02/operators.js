91a69a6b6f9299e3e0ba8087ac9a24cd79607587
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-09-20 21:14:35 -0300

    4. concatinate two strings together.

--- cse121b/week02/operators.js
+++ cse121b/week02/operators.js
@@ -3,7 +3,7 @@
 
 
 
-4. concatinate two strings together.
+
 5. assign a value to a variable
 6. increment the value in a variable by 3
 7. compare two values to see if they are the same
@@ -14,16 +14,22 @@
 
 // 1. Add two numbers
 
-const number1 = 5
-const number2 = 10
-const totalSum = number1 + number2
-console.log(`1. Add two numbers: ${number1} + ${number2} =  ${totalSum}`)
+const number1 = 5;
+const number2 = 10;
+const totalSum = number1 + number2;
+console.log(`1. Add two numbers: ${number1} + ${number2} =  ${totalSum}`);
 
 // 2. Multiply two numbers
-const totalMult = number1 * number2
-console.log(`2. Multiply two numbers: ${number1} * ${number2} = ${totalMult}`)
+const totalMult = number1 * number2;
+console.log(`2. Multiply two numbers: ${number1} * ${number2} = ${totalMult}`);
 
 // 3. Subtract one from a number
-const numberOfSubtraction = 1
-const totalSubtract = numberOfSubtraction - number1
-console.log(`3. Subtract one from a number: ${numberOfSubtraction} - ${number1} = ${totalSubtract}`)
+const numberOfSubtraction = 1;
+const totalSubtract = numberOfSubtraction - number1;
+console.log(`3. Subtract one from a number: ${numberOfSubtraction} - ${number1} = ${totalSubtract}`);
+
+//4. concatinate two strings together.
+const string1 = 'The Church of Jesus Christ';
+const string2 = 'of Latter-Day Saints';
+const totalConcat = string1 + string2;
+console.log(`4. concatinate two strings together = ${string1} ${string2}`)
