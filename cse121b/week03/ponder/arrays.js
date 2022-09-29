698abda3599cbc460116b37208b6f054d2be2ea1
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-09-29 19:43:05 -0300

    update week 03

---
+++ cse121b/week03/ponder/arrays.js
@@ -0,0 +1,13 @@
+//Activity 1 - Map
+
+// map is great when we need to change each item in an array somehow. It returns a new array and does NOT modify the original array.
+
+// let new_array = arr.map(function callback( currentValue[, index[, array]]) {
+//     // return element for new_array
+// }[, thisArg])
+// The syntax looks very similar to forEach, but notice that it needs a variable to store the new array it will return. We also need to make sure that the callback function we provide returns a value...usually the modified version of the array element.
+
+// Declare an array with value = ['one', 'two', 'three']
+// Convert our array of strings into an array of HTML strings. Each string should be turned into a list item. For example: "<li>one</li>"
+// Set our list of HTML strings into the myList list. (Hint...checkout the .join() method.)
+
