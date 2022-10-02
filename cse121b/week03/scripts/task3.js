c127fb16094778e8b3d7aab6e052a08991cdbb29
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-10-02 01:28:22 -0300

    step 1,2,3

--- cse121b/week03/scripts/task3.js
+++ cse121b/week03/scripts/task3.js
@@ -3,12 +3,21 @@
 /* FUNCTIONS */
 
 // Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
-
 // Step 2: In the function, return the sum of the parameters number1 and number2
+function add (number1, number2){
+    return number1 + number2
+}
 
 // Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
+function addNumbers(){
+    const addend1 = document.querySelector('#addend1').value;
+    const addend2 = document.querySelector('#addend2').value;
+    return add(addend1, addend2)
+}
+
 
 // Step 4: Assign the return value to an HTML form element with an ID of sum
+
 
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
 
